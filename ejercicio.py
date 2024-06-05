bd42e8aa46c258f765abf070cddcfa5a58ca1a35
bautista <bautista.lambertucci@alumnos.ispi4038.edu.ar>
2024-06-05 16:26:07 -0300

    ejercicio

---
+++ ejercicio.py
@@ -0,0 +1,5 @@
+valor_inicial = input('Ingrese valor numerico inicial:    ')
+valor_final = input('Ingrese valor numerico final:     ')
+salto = input('ingrese el salto:       ')
+secuencia = range(valor_inicial, valor_final, salto)
+print(secuencia)
